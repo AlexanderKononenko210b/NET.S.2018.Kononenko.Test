c103f5efacbad36217a141869d5915df6c4fe380
Alexander <alexander.kononenko210b@gmail.com>
2018-04-23 18:30:49 +0300

    * fix small bag

--- Task3.Solution/Bank.cs
+++ Task3.Solution/Bank.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Task3.Solution
 {
-    public class Bank : IObserver
+    public class Bank
     {
         public string Name { get; set; }
 
--- Task3.Solution/Broker.cs
+++ Task3.Solution/Broker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Task3.Solution
 {
-    public class Broker : IObserver
+    public class Broker
     {
         public string Name { get; set; }
 
