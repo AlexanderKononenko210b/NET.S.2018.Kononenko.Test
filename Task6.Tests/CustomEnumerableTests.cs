3cf1bdfcc1849bd4a28c4c5e45ff3e743d215aff
Alexander <alexander.kononenko210b@gmail.com>
2018-04-23 18:29:06 +0300

    + solving Task 6

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Test6.Solution;
 
 namespace Task6.Tests
 {
@@ -16,7 +17,12 @@ namespace Task6.Tests
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            var index = 0;
+
+            foreach (int item in Generator.GeneratorNumber(10, 1, 1, (x, y) => x + y))
+            {
+                Assert.AreEqual(expected[index++], item);
+            }
         }
 
         [Test]
@@ -24,7 +30,12 @@ namespace Task6.Tests
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            var index = 0;
+
+            foreach (int item in Generator.GeneratorNumber(10, 1, 2, (x, y) => 6 * x - 8 * y))
+            {
+                Assert.AreEqual(expected[index++], item);
+            }
         }
 
         [Test]
@@ -32,7 +43,12 @@ namespace Task6.Tests
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            var index = 0;
+
+            foreach (double item in Generator.GeneratorNumber(10, 1.0, 2.0, (x, y) => x + y / x))
+            {
+                Assert.AreEqual(expected[index++], item, 0.0000000000001);
+            }
         }
     }
 }
---
+++ Test6.Solution/Generator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6.Solution
+{
+    public static class Generator
+    {
+        public static IEnumerable<T> GeneratorNumber<T>(long number, T first, T second, Func<T,T,T> func)
+        {
+            if (number <= 1)
+                throw new ArgumentOutOfRangeException($"Argument {nameof(number)} must be more than 0");
+
+            if (func == null)
+                throw new ArgumentNullException($"Argument {nameof(func)} is null");
+
+            if (first == null)
+                throw new ArgumentNullException($"Argument {nameof(func)} is null");
+
+            if (second == null)
+                throw new ArgumentNullException($"Argument {nameof(func)} is null");
+
+            return GetNumber(number, first, second, func);
+
+        }
+
+        private static IEnumerable<T> GetNumber<T>(long number, T first, T second, Func<T, T, T> func)
+        {
+            var firstObject = first;
+
+            yield return firstObject;
+
+            var secondObject = second;
+
+            yield return secondObject;
+
+            for (int i = 3; i <= number; i++)
+            {
+                var result = func(secondObject, firstObject);
+
+                yield return result;
+
+                firstObject = secondObject;
+
+                secondObject = result;
+            }
+        }
+    }
+}
